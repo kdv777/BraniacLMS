cc04356fba2d3e0e16b69d19400e1c2ff41ddb66
KDV777 <k-d@bk.ru>
2023-01-13 15:29:02 +0500

    lesson_5 New app and user model

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Media files
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
